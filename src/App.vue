c7d617399f36d90f63c1244069ec2cfc34ba1895
Jan Doleczek <jan@doleczek.pl>
2020-08-29 22:59:56 +0200

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <v-app>
+    <hello-world />
+    <Footer />
+    <Selection />
+  </v-app>
+</template>
+
+<script>
+export default {
+  name: "App",
+  components: {
+    Footer: () => import("./components/Footer.vue"),
+    HelloWorld: () => import("./components/HelloWorld.vue"),
+    Selection: () => import("./components/Selection.vue"),
+  },
+  data() {
+    return {};
+  },
+};
+</script>
+
+<style lang="scss" scoped></style>
---
+++ vue.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  css: {
+    extract: false,
+    loaderOptions: {
+      scss: {
+        additionalData: `@import "@/styles/index.scss";`,
+      },
+    },
+  },
+  transpileDependencies: ["vuetify"],
+};
