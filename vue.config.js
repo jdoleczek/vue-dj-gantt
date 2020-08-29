50addff02850aaa02ed7ae482050bf810eff7fd5
Jan Doleczek <jan@doleczek.pl>
2020-08-30 01:51:14 +0200

    config update

--- vue.config.js
+++ vue.config.js
@@ -3,7 +3,7 @@ module.exports = {
     extract: false,
     loaderOptions: {
       scss: {
-        additionalData: `@import "@/styles/index.scss";`,
+        // additionalData: `@import "@/styles/index.scss";`,
       },
     },
   },
